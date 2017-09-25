86c1eda6f3b84ac7b1f2fc6dcbacd7d8398f3ad5
krapu1 <krapu1@hotmail.com>
2017-09-25 11:21:44 -0500

    Desarrollo del contenido 2, falta el reto.

--- js/cont02_btn03.js
+++ js/cont02_btn03.js
@@ -1,3 +1,14 @@
-$("document").ready(function(){    
+$("document").ready(function(){
     
-});
+    /* initiate pugin assigning the desired button labels  */
+    $("div.holder").jPages({
+        containerID : "itemContainer",
+        perPage     : 1,
+        first       : false,
+        previous    : "span.arrowPrev",
+        next        : "span.arrowNext",
+        last        : false
+    });
+    
+    
+});    
